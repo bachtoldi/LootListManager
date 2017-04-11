e36899d3db5a028bb8293931a24f8429d7ca0e3b
bachtoldi <bachtoldi@gmx.ch>
2017-04-11 08:15:04 +0200

    Added Angular QuickstartSeed

---
+++ Frontend/src/app/app.component/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<login-component></login-component>`,
+})
+export class AppComponent  { name = 'Angular'; }
